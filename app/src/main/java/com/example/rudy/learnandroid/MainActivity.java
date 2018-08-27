c0d0c26baec53bdc57dc189767e9dad6f6bcd640
deviation <fun.deviation@gmail.com>
2018-08-27 18:20:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/rudy/learnandroid/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.rudy.learnandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.rudy.learnandroid.learn.LearnCircle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button learnCircleBtn = findViewById(R.id.circleLearnBtn);
+        learnCircleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent learnCircle = new Intent(getApplicationContext(), LearnCircle.class);
+                startActivity(learnCircle);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/rudy/learnandroid/learn/LearnCircle.java
@@ -0,0 +1,15 @@
+package com.example.rudy.learnandroid.learn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rudy.learnandroid.R;
+
+public class LearnCircle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_circle);
+    }
+}
